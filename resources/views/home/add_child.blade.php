9db95e09f9bc90eb3acbd9e4b7679c9614a033e8
0506kinoshita <irohas.irohas@gmail.com>
2020-05-19 23:15:19 +0900

    追加、編集のバリデーションに伴う'20字以内'の文言追加

--- resources/views/home/add_child.blade.php
+++ resources/views/home/add_child.blade.php
@@ -11,20 +11,22 @@
     <div class="exist_okr" id="exist_okr">
         <h2>OBJECTIVE</h2>
         <p class="exist_okr_infomation">{{ $okr->objective }}</p>
-        <h2>KEY_RESULT</h2>
-        <p class="exist_okr_infomation">{{ $okr->key_result }}</p>
+        <h2>KEY RESULT</h2>
+        <p>{{ $okr->key_result }}</p>
     </div>
     <form action="/Home/{id}/add_child" method="post" class="add_infomation" id="add_infomation">
         {{ csrf_field() }}
-        <div class="add_okr_contents">
+        <div>
             <h2>OBJECTIVE</h2>
+                <p>*20文字以内</p>
                 @if($errors->has('objective'))
                     <p>{{ $errors->first('objective') }}</p>
                 @endif
             <input type="text" name="objective" style="width:500px; height:60px; font-size: 120%;">
         </div>
         <div class="add_okr_contents">
-            <h2>KEY_RESULT</h2>
+            <h2>KEY RESULT</h2>
+                <p>*20文字以内</p>
                 @if($errors->has('key_result'))
                     <p>{{ $errors->first('key_result') }}</p>
                 @endif
@@ -45,7 +47,7 @@
 <div class="okr_document">
     <article >
         <p><span>OBJECTIVE</span>（目標）は、シンプルで覚えやすい定性的なものを設定します</p>
-        <p><span>KEY_RESULT</span>（成果指標）は、数値化して測定し、定量的なものを設定します。</p>
+        <p><span>KEY RESULT</span>（成果指標）は、数値化して測定し、定量的なものを設定します。</p>
         <p>OKR では、目標の <span>60～70% の達成率</span>かつ、</p>
         <p>自分の自身度が<span>10分の5の難易度</span>（自信度1は絶対できない、10は簡単にできる、5はベストを尽くせばできる）がちょうど良い設定です</p>
     </article>
@@ -54,7 +56,7 @@
         <p>例1</p>
         <h1>OBJECTIVE</h1>
         <p>東京で最も人気のラーメン屋を作る</p>
-        <h1>KEY_RESULT</h1>
+        <h1>KEY RESULT</h1>
         <p>月の売り上げ1500万達成</p>
     </article>
 </div>
