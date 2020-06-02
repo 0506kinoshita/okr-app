635ef83219d5657a92f14f6df10afab9dd2cbc0c
0506kinoshita <irohas.irohas@gmail.com>
2020-06-02 11:33:48 +0900

    routeのauthのグループ化

--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,20 @@ use App\Http\Middleware\CheckSecond;
 //     return view('welcome');
 // });
 
-Route::get('/','HomeController@index')->name('home')->middleware('auth');
-Route::get('Home/add','HomeController@add')->middleware('auth');
-Route::post('Home/add','HomeController@create');
-
-Route::get('/Home/{id}','HomeController@show')->name('show')->middleware('auth');
-Route::post('Home/{id}','HomeController@delete');
-
-Route::get('Home/{id}/add_child','HomeController@add_child')->middleware('checkSecond')->middleware('auth');
-Route::post('Home/{id}/add_child','HomeController@add_child_update');
-
-Route::get('Home/{id}/edit','HomeController@edit')->middleware('auth');
-Route::post('Home/{id}/edit','HomeController@update');
+Route::group(['middleware' => 'auth'], function(){
+  Route::get('/','HomeController@index')->name('home');
+  
+  Route::get('Home/add','HomeController@add');
+  Route::post('Home/add','HomeController@create');
+  
+  Route::get('Home/{id}','HomeController@show')->name('show');
+  Route::post('Home/{id}','HomeController@delete');
+  
+  Route::get('Home/{id}/add_child','HomeController@add_child')->middleware('checkSecond');
+  Route::post('Home/{id}/add_child','HomeController@add_child_update');
+  
+  Route::get('Home/{id}/edit','HomeController@edit');
+  Route::post('Home/{id}/edit','HomeController@update');
+});
 
 Auth::routes();
